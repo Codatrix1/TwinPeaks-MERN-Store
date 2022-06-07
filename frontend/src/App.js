83122fc068ccf5099a5dbc2de3ab521075893528
Codatrix <ankur.codatrix@gmail.com>
2022-06-07 12:55:27 +0530

    React Basic Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// React Component
+const App = function () {
+  return (
+    <React.Fragment>
+      <h1>Welcome to TwinPeaks</h1>
+    </React.Fragment>
+  );
+};
+
+// Default Export
+export default App;
